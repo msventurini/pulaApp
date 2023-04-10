c784236c651bc31718c62950e559af8344cf25a1
Matheus Venturini <msventurini@inf.ufrgs.br>
2023-04-10 16:03:24 -0300

    Initial commit.

---
+++ PulaApp/PulaApp/PulaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulaAppApp.swift
+//  PulaApp
+//
+//  Created by Matheus Silveira Venturini on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PulaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
