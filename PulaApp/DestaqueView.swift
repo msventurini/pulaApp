dd25f9d58d80c7461cb7feca7fb4af4d3cc9ef8e
Felippo Stedile <felippostedile@10-41-49-83.0200.epoa.br.iosda.org>
2023-04-11 15:42:41 -0300

    Festivais e Destaques

--- PulaApp/DestaqueView.swift
+++ PulaApp/DestaqueView.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
-struct DestaqueView: View {
+struct destaqueView: View {
+    var eventName: String
+    var imageName: String
+    var date: String
+    var location: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Image(imageName)
+                .resizable()//acertar esses com as novas assets
+                .scaledToFit()//
+                .cornerRadius(8)
+                
+            Text(date)//colocar o date como texto
+                .font(.caption)
+                .fontWeight(.semibold)
+                .padding(.vertical, 1.0)
+                
+            Text(eventName)
+                .font(.body)
+                .fontWeight(.regular)
+
+            Text(location)
+                .font(.caption)
+                .fontWeight(.regular)
+                
+        }
+        .padding(.horizontal)
+        .aspectRatio(contentMode: .fit)
     }
 }
 
-struct DestaqueView_Previews: PreviewProvider {
+struct destaqueView_Previews: PreviewProvider {
     static var previews: some View {
-        DestaqueView()
+        destaqueView(eventName: "negocio", imageName: "323797023_572948307545591_7554953123518424738_n", date: "hora | dia", location: "Charqueadas")        
     }
 }
--- PulaApp/DestaquesHStack.swift
+++ PulaApp/DestaquesHStack.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct DestaquesHStack: View {
+    
+    var eventList: [Event]
+    var eventsType: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            //HStack{
+                Text(eventsType)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .padding(.bottom)
+                //Spacer()
+                //Button("Ver Tudo"){
+                    
+                //}
+                //.foregroundColor(Color(.systemBlue))
+                //.padding(.trailing)
+            //}
+            
+            ScrollView(.horizontal){
+                HStack{
+                    ForEach(eventList){ event in
+                        var date: String = event.date.formatted(.dateTime.day()) + "." + event.date.formatted(.dateTime.month(.twoDigits)) + " | " + event.date.formatted(.dateTime.hour())
+                        
+                        
+                        
+                        destaqueView(eventName: event.name, imageName: event.image, date: date, location: event.location)
+                    }
+                }
+            }
+            //.padding()
+        }
     }
 }
 
+
 struct DestaquesHStack_Previews: PreviewProvider {
     static var previews: some View {
-        DestaquesHStack()
+        DestaquesHStack(eventList: EventList.events, eventsType: "Destaques")
     }
 }
