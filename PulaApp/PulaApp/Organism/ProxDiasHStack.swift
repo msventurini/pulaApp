7f56f22f97e2b31b56115653ec48d99ae508e7f0
Felippo Stedile <felippostedile@10-41-49-83.0200.epoa.br.iosda.org>
2023-04-13 14:46:34 -0300

    Save Button Working

---
+++ PulaApp/PulaApp/Organism/ProxDiasHStack.swift
@@ -0,0 +1,44 @@
+//
+//  ProxDias.swift
+//  PulaApp
+//
+//  Created by Felippo Stedile on 12/04/23.
+//
+
+import SwiftUI
+
+struct ProxDiasHStack: View {
+    @State var eventList: [Event]
+    
+    var body: some View {
+        NavigationStack{
+            VStack(alignment: .leading){
+                HStack {
+                    Text("Nos Próximos Dias")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    NavigationLink(destination: CategoryView()){
+                        Text("Ver Tudo")//mudar o destino aqui
+                            .foregroundColor(Color.blue)
+                            .padding(.trailing)
+                    }
+                }
+                .padding(.top)
+                ScrollView(.horizontal){
+                    HStack{
+                        ForEach(eventList){ event in
+                            ProxDias(event: event)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProxDiasHStack_Previews: PreviewProvider {
+    static var previews: some View {
+        ProxDiasHStack(eventList: EventList.events)
+    }
+}
