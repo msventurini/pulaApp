7e8d8614b853b9ed5f382c7448a8336bd1d70032
Arthur Sobrosa <arthurpsl99@gmail.com>
2023-04-14 14:41:42 -0300

    Finazldkf

--- PulaApp/PulaApp/Atom/Event.swift
+++ PulaApp/PulaApp/Atom/Event.swift
@@ -12,16 +12,20 @@ class Event: ObservableObject, Identifiable {
     let name: String
     let date: Date
     let description: String
+    let description1: String
     let location: String //podemos usar o mapkit posteriormente pra colocar uma informação mais relevante sobre isso!
+    let location1: String
     let image: String
     @Published var isSaved: Bool
     
-    init(id: Int, name: String, date: Date, description: String, location: String, image: String, isSaved: Bool) {
+    init(id: Int, name: String, date: Date, description: String, description1: String, location: String, location1: String, image: String, isSaved: Bool) {
         self.id = id
         self.name = name
         self.date = date
         self.description = description
+        self.description1 = description1
         self.location = location
+        self.location1 = location1
         self.image = image
         self.isSaved = isSaved
     }
