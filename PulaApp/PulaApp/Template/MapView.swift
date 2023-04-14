73c223bce9582107bc9653097859797107390b3f
Matheus Venturini <msventurini@inf.ufrgs.br>
2023-04-14 06:49:53 -0300

    Tuning map responsiveness

--- PulaApp/PulaApp/Template/MapView.swift
+++ PulaApp/PulaApp/Template/MapView.swift
@@ -10,52 +10,51 @@ import SwiftUI
 struct MapView: View {
     var body: some View {
         
+        
         NavigationStack {
             GeometryReader { geometry in
-                ZStack {
-                    Image("mapa")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geometry.size.width)
-                                    
-                    LocalInformationView(event: EventList.events[0])
-                        .offset(
-                            x: geometry.size.width * 0.48,
-                            y: geometry.size.height * 0.05
-                        )
-                    
-                    LocalInformationView(event: EventList.events[1])
-                        .offset(
-                            x: geometry.size.width * 0.17,
-                            y: geometry.size.height * 0.215)
-                    
-                    LocalInformationView(event: EventList.events[2])
-                        .offset(
-                            x: geometry.size.width * 0.57,
-                            y: geometry.size.height * 0.38)
-                    
-                    LocalInformationView(event: EventList.events[3])
-                        .offset(
-                            x: geometry.size.width * 0.13,
-                            y: geometry.size.height * 0.5)
-
-                    
+                ScrollView(.vertical) {
+                    ZStack{
+                        Image("mapa")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: (geometry.size.width + geometry.size.height)/2)
+                                        
+                        LocalInformationView(event: EventList.events[0])
+                            .offset(
+                                x: geometry.size.width * 0.48,
+                                y: geometry.size.height * 0.05
+                            )
+                            
                         
+                        LocalInformationView(event: EventList.events[1])
+                            .offset(
+                                x: geometry.size.width * 0.17,
+                                y: geometry.size.height * 0.215)
+                        
+                        LocalInformationView(event: EventList.events[2])
+                            .offset(
+                                x: geometry.size.width * 0.57,
+                                y: geometry.size.height * 0.38)
+                        
+                        LocalInformationView(event: EventList.events[3])
+                            .offset(
+                                x: geometry.size.width * 0.13,
+                                y: geometry.size.height * 0.5)
+                    }
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height
+                    )
+                    .navigationTitle("Mapa")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbarBackground(.visible, for: .navigationBar)
                 }
-                .frame(
-                    width: geometry.size.width,
-                    height: geometry.size.height
-                )
-                .navigationTitle("Mapa")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbarBackground(.visible, for: .navigationBar)
+                .frame(height: geometry.size.height)
 
 
             }
         }
-
-        
-
     }
 }
 
